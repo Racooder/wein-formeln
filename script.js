be0db4f005f862f86ecd2de5e48d93278be700a2
simon10247 <.>
2026-03-01 03:29:31 +0100

    Added Calculation !

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function show_formula() {
     document.getElementsByClassName("hidden_formula")[0].style.display = "block";
 }
+
+function calculateNaturalAlkohol() {
+    document.querySelector('input[name="natural_alkohol"]').value = Number(document.querySelector('input[name="oechsle"]').value) * 1.25 - Number(document.querySelector('input[name="extract"]').value) * 0.5;
+}
