4f11a0b43e24db35fc830a650a573fa457783920
simon10247 <.>
2026-03-01 04:35:36 +0100

    Puh... Added a kind of js class for inputmode and contenteditable for the new input spans

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ function show_formula() {
 }
 
 // A kind of class to add attributes to the span
-
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.input_span').forEach((inputSpan) => {
+        inputSpan.setAttribute('inputmode', 'decimal');
+        inputSpan.setAttribute('contenteditable', 'true');
+    })
+})
 
 // Calculate the natural alkohol
 function calculateNaturalAlkohol() {
