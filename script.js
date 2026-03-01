0327c5d5e11e937b2591bf39ed4715b27b4f2c5c
simon10247 <.>
2026-03-01 04:26:19 +0100

    Added comments to js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Make the Lösungsweg box for vertical scroll on phone go away if the button hasn't been pressed yet
 function show_formula() {
     document.getElementsByClassName("hidden_formula")[0].style.display = "block";
 }
 
+// A kind of class to add attributes to the span
+
+
+// Calculate the natural alkohol
 function calculateNaturalAlkohol() {
     document.querySelector('input[name="natural_alkohol"]').value = Number(document.querySelector('input[name="oechsle"]').value) * 1.25 - Number(document.querySelector('input[name="extract"]').value) * 0.5;
 }
