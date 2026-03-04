623365fd529303eae77770b7f917fd45fa00b99c
simon10247 <.>
2026-03-04 21:48:05 +0100

    Put NavBar code into js, so it can be injected into every html file

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ function show_formula() {
     document.getElementsByClassName("hidden_formula")[0].style.display = "block";
 }
 
-// A kind of class to add attributes to the span
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('.input_span').forEach((inputSpan) => {
+
+    const box = document.getElementById("navigation_bar_injection");
+    box.innerHTML = `
+        <ul class="navigation_bar_ul">
+            <li class="navigation_bar_li"><img src="https://github.com/simon10247/wein-formeln/blob/main/faviconWeinFormeln.png?raw=true" alt="A wine bottle with a math formular" style="width: 40px; padding-top: 10px;"></li>
+            <li class="navigation_bar_li" style="font-weight: bold; font-size: 22px;"><a href="" style="pointer-events: none;">Wein-Formeln.de</a></li>
+            <li class="navigation_bar_break"></li>
+            <li class="navigation_bar_li"><a href=""><img class="navigation_bar_icon" src="icons/home.png" alt="Home Icone">Home</a></li>
+            <li class="navigation_bar_li"><a href=""><img class="navigation_bar_icon" src="icons/math.png" alt="Calculation Icone">Formeln</a></li>
+            <li class="navigation_bar_li"><a href=""><img class="navigation_bar_icon" src="icons/mail.png" alt="Envelope Icone">Kontakte</a></li>
+        </ul>`;
+
+    document.querySelectorAll('.input_span').forEach((inputSpan) => {   // A kind of class to add attributes to the span
         inputSpan.setAttribute('inputmode', 'decimal');
         inputSpan.setAttribute('contenteditable', 'true');
     })
