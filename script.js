a8e2db4532e23f93b6c8f5961a78d95f45d61416
simon10247 <.>
2026-03-08 16:18:35 +0100

    Moved NavBar Icons into Seperat folder Added Dropdown to Right Menu

--- script.js
+++ script.js
@@ -19,17 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
             <li class="navigation_bar_left"><img src="https://github.com/simon10247/wein-formeln/blob/main/faviconWeinFormeln.png?raw=true" alt="A wine bottle with a math formular" style="width: 40px; padding-top: 10px;"></li>
             <li class="navigation_bar_left" style="font-weight: bold; font-size: 22px;"><a href="" style="pointer-events: none;">Wein-Formeln.de</a></li>
             <li class="navigation_bar_break"></li>
-            <li class="navigation_bar_left"><a href=""><img class="navigation_bar_icon" src="icons/home.png" alt="Home Icone">Home</a></li>
+            <li class="navigation_bar_left"><a href=""><img class="navigation_bar_icon" src="icons/navigation_bar/home.png" alt="Home Icone">Home</a></li>
             <li class="navigation_bar_left navigation_bar_dropdown">
-                <a href=""><img class="navigation_bar_icon" src="icons/math.png" alt="Calculation Icone">Formeln</a>
+                <a href=""><img class="navigation_bar_icon" src="icons/navigation_bar/math.png" alt="Calculation Icone">Formeln</a>
                 <ul class="navigation_bar_dropdown_content">
                     <li><a href="">Natürlicher Alkohol</a></li>
                     <li><a href="">Anreicherung</a></li>
                     <li><a href="">[Drittes Thema hier]</a></li>
                 </ul>
             </li>
-            <li class="navigation_bar_left"><a href=""><img class="navigation_bar_icon" src="icons/mail.png" alt="Envelope Icone">Kontakte</a></li>
-            <li class="navigation_bar_right"><a href=""><img class="navigation_bar_icon" src="icons/menu.png" alt="Menu Icone"></a></li>
+            <li class="navigation_bar_left"><a href=""><img class="navigation_bar_icon" src="icons/navigation_bar/mail.png" alt="Envelope Icone">Kontakte</a></li>
+            <li class="navigation_bar_right navigation_bar_dropdown"><a href=""><img class="navigation_bar_icon" src="icons/navigation_bar/menu.png" alt="Menu Icone"></a>
+                <ul class="navigation_bar_dropdown_content_right navigation_bar_dropdown_content">
+                    <li><a href="">[Drittes Thema hier]</a></li>
+                    <li><a href="">[Drittes Thema hier]</a></li>
+                    <li><a href="">[Drittes Thema hier]</a></li>
+                </ul>
+            </li>
         </ul>`;
 
     document.querySelectorAll('.input_span').forEach((inputSpan) => {   // A kind of class to add attributes to the span
